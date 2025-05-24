2c0c9ca1238a418302f9f06df7711a8596b1e9f0
user93482 <106318293+user93482@users.noreply.github.com>
2025-05-24 16:23:46 +0200

    Update main.py

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,4 +1,29 @@
 with open('./input.txt', 'r') as f:
-  input = f.read()
+    input = f.read()
 
-print(input)
+n = input.split('\n')
+
+Fibonaccinumbers = [0,1]
+
+for i in n:
+    try:
+        i = int(i)
+        while True:
+            if Fibonaccinumbers[-1] < i:
+                Fibonaccinumbers.append(Fibonaccinumbers[-2]+Fibonaccinumbers[-1])
+            else:
+                break
+        
+        if i >= 0:
+            print(0,end='')
+            j = 1
+            while j < len(Fibonaccinumbers):
+                if Fibonaccinumbers[j] > i:
+                    break
+                elif Fibonaccinumbers[j]%3 == 0:
+                    print(', ',end='')
+                    print(Fibonaccinumbers[j],end='')
+                j += 1
+        print()
+    except:
+        print("N/A")
